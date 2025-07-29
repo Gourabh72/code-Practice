b7fc270655613009c1c701e44b79028490cf8380
Gourabh <gourabhpatel2798@gmail.com>
2025-07-29 17:04:51 +0530

    Stream Question9_10

--- streamQuestion/Question9_10.java
+++ streamQuestion/Question9_10.java
@@ -6,14 +6,20 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Question9 {
+public class Question9_10 {
     public static void main(String[] args) {
         String str="iindiiia";
+
         //find the frist non-repeated charactor
         Map<Character,Long> map=str.chars().mapToObj(c-> (char)c)
                 .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()));
         Optional<Map.Entry<Character, Long>> filterMap=map.entrySet()
                 .stream().filter(k-> k.getValue()==1).findFirst();
         filterMap.ifPresent(System.out::println);
+
+        //find the frequency of alphbet
+        str.chars().mapToObj(c-> (char)c)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet().forEach(m->System.out.print(m.getKey()+"="+m.getValue()+" "));
     }
 }
