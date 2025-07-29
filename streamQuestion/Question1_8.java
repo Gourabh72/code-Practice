7897f06979e897bd3877f9664f18bf606e491663
Gourabh <gourabhpatel2798@gmail.com>
2025-07-29 07:30:24 +0530

    Stream Question7 and 8

--- streamQuestion/Question1_8.java
+++ streamQuestion/Question1_8.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Question1 {
+public class Question1_8 {
 
     public static void main(String[] args) {
         List<Emplyee> emplyees=new ArrayList<>();
