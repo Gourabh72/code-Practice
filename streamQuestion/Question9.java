dce40c5385038577b6bba9d7ee21aab8d491b966
Gourabh <gourabhpatel2798@gmail.com>
2025-07-29 07:42:02 +0530

    Stream Question9

---
+++ streamQuestion/Question9.java
@@ -0,0 +1,19 @@
+package streamQuestion;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Question9 {
+    public static void main(String[] args) {
+        String str="iindiiia";
+        //find the frist non-repeated charactor
+        Map<Character,Long> map=str.chars().mapToObj(c-> (char)c)
+                .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()));
+        Optional<Map.Entry<Character, Long>> filterMap=map.entrySet()
+                .stream().filter(k-> k.getValue()==1).findFirst();
+        filterMap.ifPresent(System.out::println);
+    }
+}
