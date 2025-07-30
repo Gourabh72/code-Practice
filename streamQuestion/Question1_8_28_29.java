7dfdc7f4497fbdcdd1ebb445a2c3f0257f39b7a6
Gourabh <gourabhpatel2798@gmail.com>
2025-07-30 10:26:47 +0530

    Stream Question 28 and 29

--- streamQuestion/Question1_8_28_29.java
+++ streamQuestion/Question1_8_28_29.java
@@ -1,37 +1,61 @@
 package streamQuestion;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class Question1_8 {
+public class Question1_8_28_29 {
 
     public static void main(String[] args) {
         List<Emplyee> emplyees=new ArrayList<>();
-        emplyees.add(new Emplyee(1,"gourav","Male",27000));
-        emplyees.add(new Emplyee(2,"sourabh","Male",28000));
+        emplyees.add(new Emplyee(1,"Gourav","Male",27000));
+        emplyees.add(new Emplyee(2,"Sourabh","Male",28000));
         emplyees.add(new Emplyee(3,"Shubham","Male",35000));
         emplyees.add(new Emplyee(4,"Bharti","Female",10000));
         emplyees.add(new Emplyee(5,"Shivani","Female",8000));
 
         //count of male and female
+        System.out.println("count of male and female");
         emplyees.stream()
                 .collect(Collectors.groupingBy(Emplyee::getGender,Collectors.counting()))
                 .entrySet().forEach(System.out::println);
 
         //Average of female and male salary
+        System.out.println("Average of female and male salary");
         emplyees.stream()
                 .collect(Collectors.groupingBy(Emplyee::getGender,Collectors.averagingDouble(Emplyee::getSalary)))
                 .entrySet().forEach(System.out::println);
 
         //collect male and female list
+        System.out.println("collect male and female list");
         emplyees.stream()
                 .collect(Collectors.groupingBy(Emplyee::getGender,Collectors.toList()))
                 .entrySet().forEach(System.out::println);
 
         //filter who salary less then 30K
-        emplyees.stream().filter(emp-> emp.getSalary()<30000).collect(Collectors.toList())
+        System.out.println("filter who salary less then 30K");
+        emplyees.stream()
+                .filter(emp-> emp.getSalary()<30000)
+                .toList()
                 .forEach(System.out::println);
+
+        //List 'top three highest' paid employees
+        System.out.println("List 'top three highest' paid employees");
+        emplyees.stream()
+                .sorted((x,y)-> x.getSalary()<y.getSalary()?1:-1)
+                .limit(3)
+                .forEach(System.out::println);
+
+        //2nd highest salary employee
+        System.out.println("2nd highest salary employee");
+        Optional<Emplyee> emp=emplyees.stream()
+                .sorted(Comparator.comparingDouble(Emplyee::getSalary).reversed())
+                .skip(1)
+                .findFirst();
+        emp.ifPresent(System.out::print);
+
     }
 
 }
