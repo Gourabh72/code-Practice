7b56eaa53db29f0d25efeee0ef0664560a2a2e74
Gourabh <gourabhpatel2798@gmail.com>
2025-07-30 11:08:25 +0530

    Java Question Array related

---
+++ javaQuestion/Question1.java
@@ -0,0 +1,24 @@
+package javaQuestion;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Integer[] arr={3,2,1,5,8,5,3,7,9,2,4};
+
+        //list of array acending order
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i+1+":"+arr[i]);
+        }
+
+        //list of array descending order
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i+1+":"+arr[i]);
+        }
+
+    }
+}
